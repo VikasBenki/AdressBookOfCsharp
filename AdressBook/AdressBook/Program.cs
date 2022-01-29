1801641fe5d3534c4876e887e1bc76783c0b6864
VikasBenki <vikasmsbenki@gmail.com>
2022-01-30 00:07:06 +0530

    adding welcome

---
+++ AdressBook/AdressBook/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdressBook
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello welcome all to AdressBook");
+            Console.ReadLine();
+        }
+    }
+}
