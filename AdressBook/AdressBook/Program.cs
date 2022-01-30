737d39bcdfc81e0ed8e1f51443124f0952ed2aa6
VikasBenki <vikasmsbenki@gmail.com>
2022-01-30 19:29:44 +0530

    Adding data

--- AdressBook/AdressBook/Program.cs
+++ AdressBook/AdressBook/Program.cs
@@ -8,10 +8,30 @@ namespace AdressBook
 {
     internal class Program
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("hello welcome all to AdressBook");
-            Console.ReadLine();
-        }
+                        
+            //Main  Method / program Entry Point
+            static void Main(string[] args)
+            {
+                //Showing Welcome Message On console.
+                Console.WriteLine("Welcome to Address Book Progam \n");
+
+                Console.Write("Enter First Name, Last Name, Address, City, State, Zip, Phone Number, Email \n");
+                CreateAddressBook addressBook = new CreateAddressBook()  //Initializing elements using collection-initializer syntax
+                {
+                    FirstName = Console.ReadLine(),
+                    LastName = Console.ReadLine(),
+                    Address = Console.ReadLine(),
+                    City = Console.ReadLine(),
+                    State = Console.ReadLine(),
+                    Zip = Convert.ToDouble(Console.ReadLine()),
+                    PhoneNumber = Convert.ToDouble(Console.ReadLine()),
+                    Email = Console.ReadLine(),
+                };
+                addressBook.displayContact();
+                Console.ReadLine();
+            }
+        
     }
 }
+    
+
