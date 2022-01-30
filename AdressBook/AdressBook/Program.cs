3ca2c13b785bf51f5e4ff6d0df98c3c2af19ee32
VikasBenki <vikasmsbenki@gmail.com>
2022-01-30 19:40:33 +0530

    Adding the new contact

--- AdressBook/AdressBook/Program.cs
+++ AdressBook/AdressBook/Program.cs
@@ -9,28 +9,20 @@ namespace AdressBook
     internal class Program
     {
                         
+            
+        
             //Main  Method / program Entry Point
             static void Main(string[] args)
             {
                 //Showing Welcome Message On console.
                 Console.WriteLine("Welcome to Address Book Progam \n");
 
-                Console.Write("Enter First Name, Last Name, Address, City, State, Zip, Phone Number, Email \n");
-                CreateAddressBook addressBook = new CreateAddressBook()  //Initializing elements using collection-initializer syntax
-                {
-                    FirstName = Console.ReadLine(),
-                    LastName = Console.ReadLine(),
-                    Address = Console.ReadLine(),
-                    City = Console.ReadLine(),
-                    State = Console.ReadLine(),
-                    Zip = Convert.ToDouble(Console.ReadLine()),
-                    PhoneNumber = Convert.ToDouble(Console.ReadLine()),
-                    Email = Console.ReadLine(),
-                };
-                addressBook.displayContact();
+                CreateAddressBook addressBook = new CreateAddressBook();
+                addressBook.AddContact();
                 Console.ReadLine();
             }
         
+
     }
 }
     
